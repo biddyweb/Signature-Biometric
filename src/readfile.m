10aa0ff76467c5320c02f024bb3de084b0e8f39e
Victor Degliame <victor.degliame@gmail.com>
2013-08-12 02:10:32 +0200

    Update: Reformatting

--- src/readfile.m
+++ src/readfile.m
@@ -1,10 +1,9 @@
 function [C] = readfle( name )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
     fid=fopen(name);
     l = textscan(fid,'%n',1);
-    C = zeros(s, 7);
-    for i=1:s
+    nLines = l{1};
+    C = zeros(nLines, 7);
+    for i=1:nLines
         temp = textscan(fid, '%n %n %n %n %n %n %n',1);
         for j=1:7
             C(i,j)=temp{j};
--- src/script.m
+++ src/script.m
@@ -1,4 +1,3 @@
 close all;
 clear;
 main('../data/USER1_1.txt','../data/USER1_10.txt');
-
