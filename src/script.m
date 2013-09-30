7fd7afc16c8eb931be3089ab355a8bc4c08184f4
Tanguy ABEL <badjoke@badjoke>
2013-09-30 22:58:31 +0200

    added final README

--- src/script.m
+++
@@ -1,9 +0,0 @@
-close all;
-clear;
-%readfile('Reda_27092013_103517.txt');
-%readfile('Reda_27092013_103529.txt');
-%readfile('Reda_27092013_103547.txt');
-trainfunc('trainlst.txt');
-testfunc('train.mat', 'testlst.txt');
-%main('USER1_2.txt','USER1_3.txt');
-%distvect = testTrait([1], 10);
