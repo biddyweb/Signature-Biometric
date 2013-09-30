9a5304f22bee0a70d993422812187acc187b32e0
Tanguy ABEL <badjoke@badjoke>
2013-09-30 20:00:24 +0200

    fixed sign bug on decision

--- src/testfunc.m
+++ src/testfunc.m
@@ -3,8 +3,6 @@ function testfunc(nametrain, nametest)
     res2 = readtrainfile(nametest);
     [n2,m2] = size(res2);
     fid=fopen('score.txt', 'w');
-
-    score = -1000,000000;
     decisionFloor = -0.7507;
     caracFloors = [1.4130e+07, 1.3437e+03, 0.0184, 48.9787];
     coeff = [0.222, 0.1678, 0.2531, 0.3571];
@@ -22,12 +20,12 @@ function testfunc(nametrain, nametest)
         score = scores / n1;
         
         % fixme: interval
-        if score <= decisionFloor
+        if score >= decisionFloor
             decision = 't';
         else
             decision = 'f';
         end
-        fprintf(fid,'%s %s %f %s\n',name, id, score, decision); 
+        fprintf(fid,'%s %s %f %s\n', name, id, score, decision); 
     end
 
 end
